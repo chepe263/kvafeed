9304fec32d6c31dd5db03ae1d0c4e94915e0c7fa
Guillermo Chavez <gchavez@infonet.com.gt>
2017-03-15 16:40:46 -0600

    Cambiar el parser del podcast

--- public/javascripts/anapp.js
+++ public/javascripts/anapp.js
@@ -1,12 +1,34 @@
 var kvafeed = angular.module('kvafeed', []);
 
 kvafeed.controller('mainController', ['$scope','$http', function($scope, $http) {
-	$http.get('/kvafeed')
-		.success(function(data){
-			$scope.feed = data;
-			console.log(data);
-		})
-		.error(function(data) {
-            console.log('Error: ' + data);
-        });	
+	//$scope.selectedFeed = 'kva';
+	$scope.podcasts = {
+	//,{'name': '', 'label': '', 'url': ''}
+		'kva' :  {'name': 'kva', 'label': 'Euphonic Sessions with Kyau & Albert', 'url': 'http://www.kyauandalbert.com/EuphonicSessions-Podcast-KyauandAlbert.xml'}
+		,'abgt': {'name': 'abgt', 'label': 'Group Therapy with Above & Beyond', 'url': 'http://static.aboveandbeyond.nu/grouptherapy/podcast.xml'}
+	};
+	$scope.updateFeed = function(val){
+		val = val || 'kva';
+		feedUrl = $scope.podcasts[val].url;
+		cachedFeed = sessionStorage.getItem('cache_feed_' + val);
+		if (cachedFeed){
+			$scope.feed = JSON.parse(cachedFeed);
+			return;
+		}
+		$http.get('https://podcast-parser.herokuapp.com/feed?url=' + encodeURIComponent(feedUrl))
+			.success(function(data){
+				$scope.feed = data;
+				sessionStorage.setItem('cache_feed_' + val, JSON.stringify(data));
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});	
+	};
+	$scope.changeSelect = function(selectedFeed){
+		//console.log("ldld ", selectedFeed)
+		//$scope.updateFeed(document.getElementById('feed').value);
+		$scope.updateFeed(selectedFeed);
+	}
+	$scope.updateFeed('kva');
+	//document.getElementById('feed').value = "kva";
 }]);

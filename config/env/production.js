262fe5158befc8d3c26ea1cd9dfdc9195c6993fe
Jose Bautista <chepe263@gmail.com>
2019-09-22 02:19:04 -0600

    Actualizar configuracion de produccion

--- config/env/production.js
+++ config/env/production.js
@@ -250,10 +250,10 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
-    //   'https://staging.example.com',
-    // ],
+     onlyAllowOrigins: [
+      'http://kvafeed20.herokuapp.com',
+      'https://kvafeed20.herokuapp.com',
+     ],
 
 
     /***************************************************************************
